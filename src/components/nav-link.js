db3fc17b2a1fbbfe0e4cc4a319c72579d7fb0a5c
Anurag Bhandari <anurag.bhd@gmail.com>
2020-07-20 06:20:09 +0530

    Fix z-index and link styling in Header

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 const NavLink = ({ text, url, isButton }) => {
   const buttonClasses = isButton
-    ? 'btn bg-transparent hover:bg-yellow-500 border border-yellow-500 text-yellow-500 hover:text-white'
-    : '';
+    ? 'bg-blue-700 hover:bg-blue-500 text-white'
+    : 'text-gray-700 hover:text-gray-800';
 
   return (
-    <a
-      href={url}
-      className={`nav-link py-2 px-4 text-gray-700 ${buttonClasses}`}
-    >
+    <a href={url} className={`nav-link py-2 px-4 ${buttonClasses}`}>
       {text}
     </a>
   );
