52a4aeffd726a47b84d0d33dfca35927d1964503
Anurag Bhandari <anurag.bhd@gmail.com>
2020-07-20 08:08:44 +0530

    Add slight polishes to Nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,21 +4,22 @@ import NavLink from './nav-link';
 
 const Nav = () => {
   return (
-    <nav className='flex flex-row justify-between items-center'>
-      <div className='brand-logo flex'>
-        <a href='#'>
+    <nav className="nav flex flex-row justify-between items-center">
+      <div className="nav-brand flex">
+        <a href="#">
           <img
-            alt='Logo'
-            src='https://avatars3.githubusercontent.com/u/63502522?s=40&v=4'
+            alt="Logo"
+            src="https://avatars3.githubusercontent.com/u/63502522?s=40&v=4"
           />
         </a>
-        <a href='#' className='text-3xl text-blue-700'>
+        <a href="#" className="text-3xl text-blue-700">
           Universal Native
         </a>
       </div>
-      <div className='nav-links'>
-        <NavLink url='#' text='Case Studies' isButton={false} />
-        <NavLink url='#' text='Join Us' isButton={true} />
+      <div className="nav-links">
+        <NavLink url="#" text="Who We Are" isButton={false} />
+        <NavLink url="#" text="Projects" isButton={false} />
+        <NavLink url="#" text="Join Us" isButton={true} />
       </div>
     </nav>
   );
