c6624129e0f58e3b6e663dea7442c212ce57a75c
Anurag Bhandari <anurag.bhd@gmail.com>
2020-07-24 08:53:17 +0530

    Minor polishes to NavLink

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -4,12 +4,14 @@ import Link from 'next/link';
 const NavLink = ({ text, slug, isButton }) => {
   const buttonClasses = isButton
     ? 'bg-blue-700 hover:bg-blue-500 text-white'
-    : 'text-gray-700 hover:text-gray-800';
+    : 'text-gray-400 hover:text-gray-200';
 
   return (
     <Link href="/[...slug]" as={`/${slug}`}>
       <a
-        className={`nav-link block text-sm lg:text-base text-center lg:inline-block py-2 px-4 ${buttonClasses}`}
+        className={`nav-link ${
+          isButton && 'nav-button'
+        } block text-sm lg:text-base text-center lg:inline-block py-2 px-4 ${buttonClasses}`}
       >
         {text}
       </a>
